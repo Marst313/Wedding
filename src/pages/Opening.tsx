46e32facf3a7bcb410246e97fb1b3368473f8480
Marst313 <dlegoinyoman1@gmail.com>
2024-09-08 12:50:43 +0700

    bug fixing

--- src/pages/Opening.tsx
+++ src/pages/Opening.tsx
@@ -5,10 +5,10 @@ import { motion } from 'framer-motion';
 
 const Opening = () => {
   return (
-    <div className="h-screen">
+    <div className=" ">
       <Carousel />
 
-      <motion.div className="description max-w-sm mx-auto z-20 relative px-5 rounded-tr-2xl rounded-bl-2xl font-workSans" initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 2 }}>
+      <motion.div className="description max-w-sm mx-auto z-20 relative px-5 rounded-tr-2xl h-screen rounded-bl-2xl font-workSans mt-20" initial={{ scale: 0 }} animate={{ scale: 1 }} transition={{ duration: 2 }}>
         <h1 className="text-center mb-5 text-2xl capitalize  text-slate-900 font-greatVibes font-light">Dhoni & Nurul</h1>
 
         <h1 className="text-center text-slate-900 my-5  text-xl ">اَلسَّلَامُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَا تُهُ</h1>
@@ -19,8 +19,8 @@ const Opening = () => {
       <motion.img src={topRight} alt="top right" className="absolute top-0 -right-1 z-20 w-48" initial={{ x: 500 }} animate={{ x: 0 }} transition={{ duration: 2 }} />
       <motion.img src={topLeft} alt="top left" className="absolute top-0 -left-2 z-20 w-48" initial={{ x: -500 }} animate={{ x: 0 }} transition={{ duration: 2 }} />
 
-      <motion.img src={topRight} alt="bottom left" className="absolute sm:bottom-0 -left-1 z-20 rotate-180 -mt-32 w-48 " initial={{ x: -500, rotate: 180 }} animate={{ x: 0 }} transition={{ duration: 2 }} />
-      <motion.img src={topLeft} alt="bottom right" className="absolute  sm:bottom-0 -right-0 z-20 rotate-180  -mt-32 w-48" initial={{ x: 500, rotate: 180 }} animate={{ x: 0 }} transition={{ duration: 2 }} />
+      <motion.img src={topRight} alt="bottom left" className="absolute sm:bottom-0 -left-1 z-20 rotate-180 -mt-[34rem] w-48 " initial={{ x: -500, rotate: 180 }} animate={{ x: 0 }} transition={{ duration: 2 }} />
+      <motion.img src={topLeft} alt="bottom right" className="absolute  sm:bottom-0 -right-0 z-20 rotate-180  -mt-[34rem] w-48" initial={{ x: 500, rotate: 180 }} animate={{ x: 0 }} transition={{ duration: 2 }} />
     </div>
   );
 };
